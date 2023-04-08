e3f8002d396b8ff4d9644fd159763a22f2272f7e
Anjitha Mohanan <anjithamohanan263@gmail.com>
2023-04-08 19:04:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+
+const dialogflow = require('@google-cloud/dialogflow');
+const uuid = require('uuid');
+const express =require('express');
+const bodyparser =require('body-parser');
+const app = express();
+const port =5000;
+
+const sessionId = uuid.v4();
+
+app.use(bodyparser.urlencoded({
+  extended:false
+}))
+
+
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.post('/send-msg',(req,res)=>{
+  runSample(req.body.MSG).then(data=>{
+    res.send({Replay:data})
+  })
+
+})
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(msg,projectId = 'am-bot-ptlp') {
+
+ 
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+    keyFilename:"C:/Users/Anjitha/OneDrive/Desktop/chatbot/am-bot-ptlp-6a5b1c73265c.json"
+  });
+  const sessionPath = sessionClient.projectAgentSessionPath(
+    projectId,
+    sessionId
+  );
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: msg,
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log('  No intent matched.');
+  }
+  return result.fulfillmentText;
+}
+app.listen(port,()=>{
+  console.log("running on port" +port);
+})
